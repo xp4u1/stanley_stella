04d0a596afde9d5a5ad9024920d2053ef6d57102
Paul Hoerenz <development@hoerenz.com>
2023-12-15 13:37:49 +0100

    ✨ Add color finder

--- lib/stanley_stella/data/color.ex
+++ lib/stanley_stella/data/color.ex
@@ -5,4 +5,33 @@ defmodule StanleyStella.Data.Color do
     :name,
     :image
   ]
+
+  @spec color_by_id(String.t()) :: StanleyStella.Data.Color | nil
+  def color_by_id(id) do
+    list_colors()
+    |> Enum.filter(&(&1.id == id))
+    |> Enum.at(0)
+  end
+
+  @spec list_colors() :: list(StanleyStella.Data.Color)
+  def list_colors do
+    HTTPoison.get!("https://www.stanleystella.com/de-de/colour-card").body
+    |> Floki.parse_document!()
+    |> Floki.find(".colorEntry")
+    |> Enum.map(fn element ->
+      id =
+        element
+        |> Floki.attribute("data-colorid")
+        |> Enum.at(0)
+
+      %StanleyStella.Data.Color{
+        id: id,
+        name:
+          element
+          |> Floki.attribute("data-colorname")
+          |> Enum.at(0),
+        image: "https://www.stanleystella.com/content/files/images/ColorImages/#{id}.png"
+      }
+    end)
+  end
 end
